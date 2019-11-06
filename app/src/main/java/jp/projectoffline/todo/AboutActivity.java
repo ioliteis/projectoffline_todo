3eb8f6a8a71958b2da7cc4fa021b063eb0dedf2f
iolite <iris@eay.jp>
2019-11-06 20:37:23 +0900

    New About screen. Bug fixed.

--- app/src/main/java/jp/projectoffline/todo/AboutActivity.java
+++ app/src/main/java/jp/projectoffline/todo/AboutActivity.java
@@ -2,84 +2,156 @@ package jp.projectoffline.todo;
 
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.preference.PreferenceManager;
 
-public class AboutActivity extends AppCompatActivity {
+import com.danielstone.materialaboutlibrary.MaterialAboutActivity;
+import com.danielstone.materialaboutlibrary.items.MaterialAboutActionItem;
+import com.danielstone.materialaboutlibrary.items.MaterialAboutItemOnClickAction;
+import com.danielstone.materialaboutlibrary.items.MaterialAboutTitleItem;
+import com.danielstone.materialaboutlibrary.model.MaterialAboutCard;
+import com.danielstone.materialaboutlibrary.model.MaterialAboutList;
+import com.mikepenz.community_material_typeface_library.CommunityMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
 
-    String string1;
-    String string2;
+public class AboutActivity extends MaterialAboutActivity {
+    int  colorIcon = R.color.colorAccent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //ダークテーマ
+        //Dark theme
         if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("dark_theme", true)){
-            setTheme(R.style.AppTheme_Dark);
+            setTheme(R.style.Theme_Mal_Dark);
         }
-        setContentView(R.layout.activity_about);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        Button button1 = findViewById(R.id.button1);
-        Button button2 = findViewById(R.id.button2);
-        Button button3 = findViewById(R.id.button3);
-        Button button4 = findViewById(R.id.button4);
-
-        string1 = getString(R.string.about_version);
-        string2 = getString(R.string.about_copy);
-
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ClipboardManager clipboardManager = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
-                clipboardManager.setPrimaryClip(ClipData.newPlainText("", string1));
-
-                Toast.makeText(AboutActivity.this, string2, Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri1 = Uri.parse("https://github.com/ioliteis/projectoffline_todo");
-                Intent a = new Intent(Intent.ACTION_VIEW, uri1);
-                startActivity(a);
-            }
-        });
-
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri2 = Uri.parse("https://ioliteis.github.io/projectoffline/2019/08/25/Privacy-Policy/");
-                Intent b = new Intent(Intent.ACTION_VIEW, uri2);
-                startActivity(b);
-            }
-        });
-
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri3 = Uri.parse("https://ioliteis.github.io/projectoffline");
-                Intent c = new Intent(Intent.ACTION_VIEW, uri3);
-                startActivity(c);
-            }
-        });
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        finish();
-        return super.onSupportNavigateUp();
+    @NonNull
+    protected MaterialAboutList getMaterialAboutList(@NonNull final Context c) {
+        MaterialAboutCard.Builder cardBuilder1 = new MaterialAboutCard.Builder();
 
+
+        cardBuilder1.addItem(new MaterialAboutTitleItem.Builder()
+                .text(R.string.about_about)
+                .icon(R.mipmap.ic_launcher)
+                .build());
+
+        cardBuilder1.addItem(new MaterialAboutActionItem.Builder()
+                .text(getString(R.string.about_version))
+                .subText(getString(R.string.version))
+                .icon(new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon2.cmd_information_outline)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18))
+                .setOnClickAction(new MaterialAboutItemOnClickAction() {
+                    @Override
+                    public void onClick() {
+                        ClipboardManager clipboardManager = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+                        clipboardManager.setPrimaryClip(ClipData.newPlainText("", getString(R.string.about_version) + " " + getString(R.string.version)));
+
+                        Toast.makeText(AboutActivity.this, getString(R.string.about_copy), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .build());
+
+        cardBuilder1.addItem(new MaterialAboutActionItem.Builder()
+                .text(getString(R.string.about_source))
+                .icon(new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon.cmd_github_circle)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18))
+                .setOnClickAction(new MaterialAboutItemOnClickAction() {
+                    @Override
+                    public void onClick() {
+                        Uri uri1 = Uri.parse("https://github.com/ioliteis/projectoffline_todo");
+                        Intent a = new Intent(Intent.ACTION_VIEW, uri1);
+                        startActivity(a);
+                    }
+                })
+                .build());
+
+        cardBuilder1.addItem(new MaterialAboutActionItem.Builder()
+                .text(getString(R.string.about_licenses))
+                .icon(new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon.cmd_book)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18))
+                .setOnClickAction(new MaterialAboutItemOnClickAction() {
+                    @Override
+                    public void onClick() {
+                        Intent intent = new android.content.Intent(AboutActivity.this, LicensesActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .build());
+
+        MaterialAboutCard.Builder cardBuilder2 = new MaterialAboutCard.Builder();
+
+        cardBuilder2.addItem(new MaterialAboutActionItem.Builder()
+                .text(getString(R.string.about_po))
+                .subText(getString(R.string.about_website))
+                .icon(new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon.cmd_flask)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18))
+                .setOnClickAction(new MaterialAboutItemOnClickAction() {
+                    @Override
+                    public void onClick() {
+                        Uri uri2 = Uri.parse("https://ioliteis.github.io/projectoffline");
+                        Intent c = new Intent(Intent.ACTION_VIEW, uri2);
+                        startActivity(c);
+                    }
+                })
+                .build());
+
+        cardBuilder2.addItem(new MaterialAboutActionItem.Builder()
+                .text(getString(R.string.about_privacy))
+                .subText(getString(R.string.about_privacy_comment))
+                .icon(new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon2.cmd_shield_half_full)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18))
+                .setOnClickAction(new MaterialAboutItemOnClickAction() {
+                    @Override
+                    public void onClick() {
+                        Uri uri3 = Uri.parse("https://ioliteis.github.io/projectoffline/2019/08/26/Privacy-Policy/");
+                        Intent b = new Intent(Intent.ACTION_VIEW, uri3);
+                        startActivity(b);
+                    }
+                })
+                .build());
+
+        cardBuilder2.addItem(new MaterialAboutActionItem.Builder()
+                .text(getString(R.string.about_mastodon))
+                .subText(getString(R.string.about_contact))
+                .icon(new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon2.cmd_mastodon)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18))
+                .setOnClickAction(new MaterialAboutItemOnClickAction() {
+                    @Override
+                    public void onClick() {
+                        Uri uri4 = Uri.parse("https://social.privacytools.io/@projectoffline");
+                        Intent b = new Intent(Intent.ACTION_VIEW, uri4);
+                        startActivity(b);
+                    }
+                })
+                .build());
+
+        return  new MaterialAboutList(cardBuilder1.build(), cardBuilder2.build());
     }
+
+    @Override
+    protected CharSequence getActivityTitle() {
+        return getString(R.string.action_about);
+    }
+
 }
